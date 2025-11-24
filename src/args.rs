cf968ca123b830c39d649fb9b38af4f9f94d81b4
mlam31 <mathieu.lam31@gmail.com>
2025-11-24 14:56:04 +0100

    update: clap for transaction

--- src/args.rs
+++ src/args.rs
@@ -1,16 +1,44 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use crate::blockchain::{Block, Blockchain, Transaction, TransactionPool};
 
 #[derive(Parser, Debug)]
 pub struct BlockchainArgs{
-    /// Transaction, Block, TransactionPool, Blockchain
-    pub entity_type: String,
-    /// Commands
-    pub entity_command: String,
-    /// Subcommands
-    pub entity_subcommands: String
+    #[command(subcommand)]
+    pub entity: EntityType,
 }
 
-// Command for transaction: create, add
+#[derive(Debug, Subcommand)]
+pub enum EntityType {
+    Block,
+    Blockchain,
+    Transaction {
+        #[command(subcommand)]
+        command: TransactionCommand,
+    },
+    TransactionPool
+}
+
+#[derive(Subcommand, Debug)]
+pub enum TransactionCommand {
+    Create(TransactionArgs)
+}
+
+#[derive(Parser, Debug)]
+pub struct TransactionArgs {
+    /// The one who sends the amount
+    #[arg(short, long)]
+    pub sender: Option<String>,
+    /// The one who receives the amount
+    #[arg(short, long)]
+    pub receiver: Option<String>,
+    /// The amount 
+    #[arg(short, long)]
+    pub amout: Option<f64>,
+}
+
+impl TransactionArgs {}
+
+// Command for transaction: create
 // Commands for block: create, mine
 // Commands for transaction_pool: status, clear
 // Commands for blockchain: status
